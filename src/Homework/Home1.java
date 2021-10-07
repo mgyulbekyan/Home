32223872a1dd1df5afb7792f995861b08c0a5c0a
Mariam Gyulbekyan <gyulbekyanm94@mail.ru>
2021-10-07 23:34:31 +0500

    1

---
+++ src/Homework/Home1.java
@@ -0,0 +1,17 @@
+package Homework;
+
+public class Home1 {
+    public static void main(String[] args) {
+        for (int i = 4; i>0; i--) {
+            for (int j =0; j<i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
+
+
